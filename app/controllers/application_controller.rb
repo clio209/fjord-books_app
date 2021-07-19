d5772dd9e12b73965f0e52449b2abb1a70258000
honda <clio209@gmail.com>
2021-07-20 08:24:31 +0900

    view devise を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user! 
 end
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+end
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+end
