ce8e26bfba777af020b72b160cdeb8dbc49f33d2
honda <clio209@gmail.com>
2021-07-28 16:29:46 +0900

    viewsにdeviseとusersを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user! 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
 end
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-end
-
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-end
---
+++ db/migrate/20210727113242_add_postal_to_users.rb
@@ -0,0 +1,5 @@
+class AddPostalToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :postal, :string
+  end
+end
