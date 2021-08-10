a08b039ee0a4914086aaf2af4c335ed8c6733041
honda <clio209@gmail.com>
2021-08-10 21:59:33 +0900

    登録画面(new)migrationファイルの修正

--- db/migrate/20210727113242_add_postal_to_users.rb
+++ db/migrate/20210727113242_add_postal_to_users.rb
@@ -2,7 +2,7 @@
 
 class AddPostalToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :name, :string null: false
+    add_column :users, :name, :string, null: false
     add_column :users, :postal, :string
     add_column :users, :address, :text
     add_column :users, :description, :text
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_113524) do
+ActiveRecord::Schema.define(version: 2021_07_27_113242) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_113524) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", null: false
     t.string "postal"
     t.text "address"
     t.text "description"
