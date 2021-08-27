42ddb7db0f5ee7b707a075683a62c629e1474d74
honda <clio209@gmail.com>
2021-08-27 11:29:57 +0900

    コメント投稿機能の実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,54 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  # GET /comments
+  def index
+    @comments = Comment.all
+  end
+
+  # GET /comments/1
+  def show
+  end
+
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/1/edit
+  def edit
+  end
+
+  # POST /comments
+  def create
+    @comment = @commentable.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to @commentable, notice: 'Comment was successfully created.'
+  end
+
+  # PATCH/PUT /comments/1
+  def update
+    if @comment.update(comment_params)
+      redirect_to @comment, notice: 'Comment was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /comments/1
+  def destroy
+    @comment.destroy
+    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
