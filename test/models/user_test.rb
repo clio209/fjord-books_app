9f3c9d2c962810c7b2d7e0306263b94aea97e95f
honda <clio209@gmail.com>
2021-08-31 12:07:18 +0900

    準備完了状態

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#name_or_email" do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal = assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo Bar'
+    assert_equal 'Foo Bar' , user.name_or_email
+  end
+
+  test "#follow" do
+    me = User.create!(email: 'me@example.com, password: 'password)
+    he = User.create!(email: 'he@example.com, password: 'password)
+
+    assert me.follow
+  end
 end
